0671fb738860fecd679ca4af5ec99d5dbad322dc
larryTheCoder <larryPeter132@gmail.com>
2021-07-27 22:24:26 +0800

    [Build] Introduce UHC preset to VanillaGenerator.

--- src/muqsit/vanillagenerator/generator/overworld/OverworldGenerator.php
+++ src/muqsit/vanillagenerator/generator/overworld/OverworldGenerator.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace muqsit\vanillagenerator\generator\overworld;
 
 use muqsit\vanillagenerator\generator\Environment;
-use muqsit\vanillagenerator\generator\overworld\populator\CavePopulator;
 use muqsit\vanillagenerator\generator\overworld\populator\OverworldPopulator;
 use muqsit\vanillagenerator\generator\overworld\populator\SnowPopulator;
 use muqsit\vanillagenerator\generator\utils\WorldOctaves;
@@ -27,7 +26,28 @@ class OverworldGenerator extends VanillaGenerator
 		parent::__construct($seed, Environment::OVERWORLD, null, $preset);
 		$this->addPopulators(new OverworldPopulator(), new SnowPopulator());
 
-		$this->generator = new \OverworldGenerator($seed);
+		$enableUHC = false;
+
+		// The preset example: "isUHC,1:environment,overworld"
+		$presets = explode(':', $preset);
+		foreach ($presets as $preset) {
+			$settings = explode(',', $preset);
+			if (count($settings) < 2) {
+				throw new InvalidArgumentException("World preset must have a key and a value respectively");
+			}
+
+			switch ($settings[0]) {
+				case "isUHC":
+					$enableUHC = (int)$settings[1] === 1;
+					break;
+				case "environment":
+				case "amplification":
+					// TODO: These presets are available in mc-generator but they remain inaccessible for now.
+			}
+		}
+
+		/** @phpstan-ignore-next-line */
+		$this->generator = new \OverworldGenerator($seed, $enableUHC);
 	}
 
 	public function generateChunk(ChunkManager $world, int $chunkX, int $chunkZ): void
@@ -52,7 +72,7 @@ class OverworldGenerator extends VanillaGenerator
 
 		(function () use ($biomes): void {
 			/** @noinspection PhpUndefinedFieldInspection */
-			/** @phpstan-ignore-next-line  */
+			/** @phpstan-ignore-next-line */
 			$this->biomeIds = new BiomeArray($biomes);
 		})->call($chunk);
 	}
